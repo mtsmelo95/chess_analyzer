9e941101a2c0babef639eee66aa9db96248a658d
mtsmelo95 <mtsmelo95@gmail.com>
2025-06-29 22:59:54 -0300

    testing

--- app/controllers/game_controller.py
+++ app/controllers/game_controller.py
@@ -3,4 +3,5 @@ from app.services.analysis_service import analyze_pgn
 
 def upload_and_analyze(file: UploadFile):
     pgn_content = file.file.read().decode("utf-8")
+    print(f"Received PGN content: {pgn_content[:100]}...")
     return analyze_pgn(pgn_content)
